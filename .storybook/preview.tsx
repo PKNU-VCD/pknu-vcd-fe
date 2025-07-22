7dd71f2fa5fc9fe7bcce319851523f3dd85773eb
H0ngJu <hongju4385@gmail.com>
2025-07-22 22:00:36 +0900

    chore: storybook 설치

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from '@emotion/react';
+import type { Preview } from '@storybook/react';
+import React from 'react';
+import GlobalStyle from '../src/styles/globalStyles';
+import { theme } from '../src/styles/theme';
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Story />
+        </>
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
---
+++ src/stories/page.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import HomePage from '../app/page';
+
+const meta: Meta<typeof HomePage> = {
+  title: 'Pages/HomePage',
+  component: HomePage,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof HomePage>;
+
+export const Default: Story = {};
