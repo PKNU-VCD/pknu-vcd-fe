925b1f84d0be14852de56e1803a057ce59e974f0
H0ngJu <hongju4385@gmail.com>
2025-07-27 17:36:41 +0900

    feat: 랜덤 컬러 추출 함수 추가

---
+++ src/utils/randomColor.ts
@@ -0,0 +1,5 @@
+export const getRandomThemeColor = <T extends readonly string[]>(colors: T): T[number] => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+  
