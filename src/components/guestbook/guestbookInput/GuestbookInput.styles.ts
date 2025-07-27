8783f6f600a9a2b0ffc3cf99d924f9c362768306
H0ngJu <hongju4385@gmail.com>
2025-07-28 00:31:58 +0900

    feat: 100글자 이상인 경우 모션 추가 및 제출 onSubmit 수정

--- src/components/guestbook/guestbookInput/GuestbookInput.styles.ts
+++ src/components/guestbook/guestbookInput/GuestbookInput.styles.ts
@@ -1,17 +1,52 @@
+import { css, keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   position: relative;
   display: flex;
-  align-items: flex-start;
+  gap: 20px;
+  padding: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    flex-direction: column;
+    flex-direction: column-reverse;
+  }
 `;
 
-export const TextAreaContainer = styled.textarea`
+const shake = keyframes`
+  0% { transform: translateX(0); }
+  20% { transform: translateX(-10px); }
+  40% { transform: translateX(10px); }
+  60% { transform: translateX(-10px); }
+  80% { transform: translateX(10px); }
+  100% { transform: translateX(0); }
+`;
+
+export const TextAreaContainer = styled.textarea<{ $isOverLimit: boolean }>`
   width: 100%;
   border: 1px solid ${({ theme }) => theme.colors.input};
   ${({ theme }) => theme.typography.regular};
   border-radius: 20px;
   padding: 30px 30px 60px 30px;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: gray;
+    border-radius: 8px;
+  }
+
+  ${({ $isOverLimit }) =>
+    $isOverLimit &&
+    css`
+      animation: ${shake} 0.4s ease-in-out;
+    `}
 `;
 
 export const TextLengthContainer = styled.div<{ $isOverLimit: boolean }>`
@@ -21,7 +56,11 @@ export const TextLengthContainer = styled.div<{ $isOverLimit: boolean }>`
     $isOverLimit ? theme.typography.extraBold : theme.typography.regular};
   position: absolute;
   bottom: 12px;
-  right: 30px;
+  right: 80px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    right: 20px;
+  }
 `;
 
 export const ButtonContainer = styled.button`
@@ -29,4 +68,5 @@ export const ButtonContainer = styled.button`
   border: 1px solid #f2f2f2;
   display: inline-flex;
   padding: 10px;
+  align-self: flex-end;
 `;
