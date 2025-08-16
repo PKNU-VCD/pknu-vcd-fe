e31d3137173029041bb06601589d8b1061f9b7a1
H0ngJu <hongju4385@gmail.com>
2025-08-16 23:15:07 +0900

    feat: /로 들어오면, main으로 들어오도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { GuestbookInput } from '@/components/guestbook/guestbookInput/GuestbookInput';
-import GlobalStyle from '@/styles/globalStyles';
-import * as S from './page.styles';
-
-export default function HomePage() {
-  return (
-    <>
-      <GlobalStyle />
-      <S.Container>
-        <S.Title>Hello, world!</S.Title>
-        <S.Description>Emotion 스타일 테스트입니다.</S.Description>
-        <GuestbookInput onSubmit={() => {}} />
-      </S.Container>
-    </>
-  );
+export default function Page() {
+  redirect('/main');
 }
