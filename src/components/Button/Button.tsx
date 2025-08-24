48ba0b409b45ca46d7beccc9e2c3fdba45c3c437
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-08-24 14:44:42 +0900

    fix: button폴더명을 Button으로 변경

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { BaseButton } from '@/components/button/Button.styles';
+import { BaseButton } from '@/components/Button/Button.styles';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?:
