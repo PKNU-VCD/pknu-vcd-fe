aff505cb8f5eff39676a29e5c953fb32d676fd96
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-08-25 16:25:39 +0900

    feat: 소개합니다 버튼 리스트 컴포넌트 추가
    - useRouter로 라우팅 설정

---
+++ src/components/buttonlist/ButtonList.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/navigation';
+import { Button } from '../button/Button';
+import * as S from './ButtonList.styles';
+
+export default function ButtonList() {
+  const router = useRouter();
+
+  const handleButtonClick = (path: string) => {
+    router.push(path);
+  };
+
+  return (
+    <S.ButtonContainer>
+      <Button
+        variant="tertiary"
+        label="전시 소개"
+        onClick={() => handleButtonClick('/introduce/introduction')}
+      />
+      <Button
+        variant="tertiary"
+        label="전시 상세"
+        onClick={() => handleButtonClick('/introduce/detail')}
+      />
+      <Button
+        variant="tertiary"
+        label="축사"
+        onClick={() => handleButtonClick('/introduce/celebration')}
+      />
+    </S.ButtonContainer>
+  );
+}
