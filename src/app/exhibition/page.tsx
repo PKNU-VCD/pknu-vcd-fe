79a8bede3344dd7e7a2cfd8f8dcb3322968f68d0
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-08-25 16:23:42 +0900

    feat: 전시합니다 페이지 UI 추가

---
+++ src/app/exhibition/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Header from '@/components/header/Header';
+
+export default function Exhibition() {
+  return (
+    <>
+      <Header headerType="main" />
+      <div>Exhibition</div>
+    </>
+  );
+}
