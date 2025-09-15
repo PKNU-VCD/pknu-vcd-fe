b5d084d1a44b77aaecac51ce6cda167e9148ebf5
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-09-15 10:33:50 +0900

    feat: 디자인합니다 페이지 UI 및 스타일 추가

---
+++ src/app/architect/page.styles.ts
@@ -0,0 +1,41 @@
+import { theme } from '@/styles/theme';
+import styled from '@emotion/styled';
+import { CommonWrapper } from '../introduce/common.styles';
+
+export const DesignerContainer = styled(CommonWrapper)`
+  gap: 60px;
+`;
+
+export const LogoText = styled.p`
+  color: ${theme.colors.stroke};
+  font-size: ${theme.typography.medium.fontSize};
+  font-weight: ${theme.typography.medium.fontWeight};
+  line-height: ${theme.typography.medium.lineHeight};
+  letter-spacing: ${theme.typography.medium.letterSpacing};
+`;
+
+export const LogoWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const UnionWrapper = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 14%;
+  transform: translate(-50%, -50%);
+`;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  padding: 10px 100px;
+`;
+
+export const MenuListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 60px 100px;
+`;
+
--- src/app/architect/page.tsx
+++ src/app/architect/page.tsx
@@ -1,12 +1,37 @@
 'use client';
 
+import IntroLogo from '@/assets/icons/sub/intro.svg';
+import DesignUnion from '@/assets/icons/sub/Union_design.svg';
+import ButtonList from '@/components/buttonlist/architect/ButtonList';
+import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
+import { SearchBar } from '@/components/searchBar/SearchBar';
+import { ThumbnailGrid } from '@/components/thumbnailGrid/ThumbnailGrid';
+import * as S from './page.styles';
+
 
 export default function Architect() {
   return (
     <>
       <Header headerType="main" />
-      <div>Architect</div>
+      <S.DesignerContainer>
+        <S.LogoText>
+          제 37회 2025 국립부경대학교 시각디자인전공 졸업전시회 디자인 프로젝트
+        </S.LogoText>
+        <S.LogoWrapper>
+          <IntroLogo />
+          <S.UnionWrapper>
+            <DesignUnion />
+          </S.UnionWrapper>
+        </S.LogoWrapper>
+      </S.DesignerContainer>
+      <S.SearchContainer>
+        <SearchBar />
+      </S.SearchContainer>
+      <S.MenuListContainer>
+        <ButtonList />
+      </S.MenuListContainer>
+      <Footer footerType="sub" />
     </>
   );
 }
