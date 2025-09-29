2ddd1d63258fe905a889695e9d4014f8c365fd72
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-09-29 16:31:45 +0900

    fix: 충돌해결을 위한 덮어쓰기

--- src/components/buttonlist/introduce/ButtonList.tsx
+++ src/components/buttonlist/introduce/ButtonList.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/Button/Button';
-import { useNavigator } from '@/hooks/useNavigator';
+import { usePathname, useRouter } from 'next/navigation';
 import * as S from './ButtonList.styles';
 
 export default function ButtonList() {
-  const { navigateTo } = useNavigator();
+  const router = useRouter();
+  const pathname = usePathname();
 
   const handleButtonClick = (path: string) => {
-    navigateTo(path);
+    router.push(path);
   };
 
   return (
@@ -14,16 +15,19 @@ export default function ButtonList() {
       <Button
         variant="tertiary"
         label="전시 소개"
+        isActive={pathname === '/introduce/introduction'}
         onClick={() => handleButtonClick('/introduce/introduction')}
       />
       <Button
         variant="tertiary"
         label="전시 상세"
+        isActive={pathname === '/introduce/detail'}
         onClick={() => handleButtonClick('/introduce/detail')}
       />
       <Button
         variant="tertiary"
         label="축사"
+        isActive={pathname === '/introduce/celebration'}
         onClick={() => handleButtonClick('/introduce/celebration')}
       />
     </S.ButtonContainer>
