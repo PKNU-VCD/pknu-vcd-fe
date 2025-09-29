5d7d4f0af2281d3ecdfb7bb53997065b22cd0200
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-09-29 16:23:33 +0900

    Revert "fix: 2차 충돌해결"
    This reverts commit 58321e400f4dfd77d26d8fcc160cbbca6d915a68.

--- src/components/buttonlist/architect/ButtonList.tsx
+++ src/components/buttonlist/architect/ButtonList.tsx
@@ -1,14 +1,17 @@
 import { Button } from '@/components/Button/Button';
+import { useNavigator } from '@/hooks/useNavigator';
 import { useState } from 'react';
 import * as S from './ButtonList.styles';
 
 export default function ButtonList() {
+  const { navigateTo } = useNavigator();
   const [selectedButtons, setSelectedButtons] = useState<string[]>([]);
 
-  const handleButtonClick = (label: string) => {
+  const handleButtonClick = (label: string, path: string) => {
     setSelectedButtons(prev =>
       prev.includes(label) ? prev.filter(item => item !== label) : [...prev, label],
     );
+    navigateTo(path);
   };
 
   return (
@@ -16,37 +19,37 @@ export default function ButtonList() {
       <Button
         variant="secondary"
         label="Branding"
-        onClick={() => handleButtonClick('Branding')}
+        onClick={() => handleButtonClick('Branding', '/architect/branding')}
         isActive={selectedButtons.includes('Branding')}
       />
       <Button
         variant="secondary"
         label="Package"
-        onClick={() => handleButtonClick('Package')}
+        onClick={() => handleButtonClick('Package', '/architect/package')}
         isActive={selectedButtons.includes('Package')}
       />
       <Button
         variant="secondary"
         label="Graphic"
-        onClick={() => handleButtonClick('Graphic')}
+        onClick={() => handleButtonClick('Graphic', '/architect/graphic')}
         isActive={selectedButtons.includes('Graphic')}
       />
       <Button
         variant="secondary"
         label="illustration"
-        onClick={() => handleButtonClick('illustration')}
+        onClick={() => handleButtonClick('illustration', '/architect/illustration')}
         isActive={selectedButtons.includes('illustration')}
       />
       <Button
         variant="secondary"
         label="UI/UX"
-        onClick={() => handleButtonClick('UI/UX')}
+        onClick={() => handleButtonClick('UI/UX', '/architect/ui-ux')}
         isActive={selectedButtons.includes('UI/UX')}
       />
       <Button
         variant="secondary"
         label="Editorial"
-        onClick={() => handleButtonClick('Editorial')}
+        onClick={() => handleButtonClick('Editorial', '/architect/editorial')}
         isActive={selectedButtons.includes('Editorial')}
       />
     </S.ButtonContainer>
--- src/components/buttonlist/introduce/ButtonList.tsx
+++ src/components/buttonlist/introduce/ButtonList.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/Button/Button';
 import { useNavigator } from '@/hooks/useNavigator';
+import { usePathname } from 'next/navigation';
 import * as S from './ButtonList.styles';
 
 export default function ButtonList() {
+  const pathname = usePathname();
   const { navigateTo } = useNavigator();
 
   const handleButtonClick = (path: string) => {
@@ -14,16 +16,19 @@ export default function ButtonList() {
       <Button
         variant="tertiary"
         label="전시 소개"
+        isActive={pathname === '/introduce/introduction'}
         onClick={() => handleButtonClick('/introduce/introduction')}
       />
       <Button
         variant="tertiary"
         label="전시 상세"
+        isActive={pathname === '/introduce/detail'}
         onClick={() => handleButtonClick('/introduce/detail')}
       />
       <Button
         variant="tertiary"
         label="축사"
+        isActive={pathname === '/introduce/celebration'}
         onClick={() => handleButtonClick('/introduce/celebration')}
       />
     </S.ButtonContainer>
