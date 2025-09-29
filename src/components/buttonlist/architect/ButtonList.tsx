58321e400f4dfd77d26d8fcc160cbbca6d915a68
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-09-29 16:21:27 +0900

    fix: 2차 충돌해결

--- src/components/buttonlist/architect/ButtonList.tsx
+++ src/components/buttonlist/architect/ButtonList.tsx
@@ -1,17 +1,14 @@
 import { Button } from '@/components/Button/Button';
-import { useNavigator } from '@/hooks/useNavigator';
 import { useState } from 'react';
 import * as S from './ButtonList.styles';
 
 export default function ButtonList() {
-  const { navigateTo } = useNavigator();
   const [selectedButtons, setSelectedButtons] = useState<string[]>([]);
 
-  const handleButtonClick = (label: string, path: string) => {
+  const handleButtonClick = (label: string) => {
     setSelectedButtons(prev =>
       prev.includes(label) ? prev.filter(item => item !== label) : [...prev, label],
     );
-    navigateTo(path);
   };
 
   return (
@@ -19,37 +16,37 @@ export default function ButtonList() {
       <Button
         variant="secondary"
         label="Branding"
-        onClick={() => handleButtonClick('Branding', '/architect/branding')}
+        onClick={() => handleButtonClick('Branding')}
         isActive={selectedButtons.includes('Branding')}
       />
       <Button
         variant="secondary"
         label="Package"
-        onClick={() => handleButtonClick('Package', '/architect/package')}
+        onClick={() => handleButtonClick('Package')}
         isActive={selectedButtons.includes('Package')}
       />
       <Button
         variant="secondary"
         label="Graphic"
-        onClick={() => handleButtonClick('Graphic', '/architect/graphic')}
+        onClick={() => handleButtonClick('Graphic')}
         isActive={selectedButtons.includes('Graphic')}
       />
       <Button
         variant="secondary"
         label="illustration"
-        onClick={() => handleButtonClick('illustration', '/architect/illustration')}
+        onClick={() => handleButtonClick('illustration')}
         isActive={selectedButtons.includes('illustration')}
       />
       <Button
         variant="secondary"
         label="UI/UX"
-        onClick={() => handleButtonClick('UI/UX', '/architect/ui-ux')}
+        onClick={() => handleButtonClick('UI/UX')}
         isActive={selectedButtons.includes('UI/UX')}
       />
       <Button
         variant="secondary"
         label="Editorial"
-        onClick={() => handleButtonClick('Editorial', '/architect/editorial')}
+        onClick={() => handleButtonClick('Editorial')}
         isActive={selectedButtons.includes('Editorial')}
       />
     </S.ButtonContainer>
